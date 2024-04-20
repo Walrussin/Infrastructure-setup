711e44b810033b3d66d51ba38e7a2cac0ef73050
Walton McDonald <walrussin@mailfence.com>
2024-04-20 13:47:03 -0600

    Adding terraform to IAC machine

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Infrastructure-setup
-Setup the infrastructure for a simple-web server. Create Resources, Harden, Deploy
+Setup the infrastructure for a simple web server. Create Resources, Harden, Deploy
+
+
+### Harden
+
+Ansible provides means of checking compliance without enforcement called --check (aka “dry
+run”). To use this mode, run the following:
+
+`ansible-playbook -v -b -i /dev/null --check site.yml`
---
+++ provision/terrafrom/main.tf
@@ -0,0 +1,24 @@
+# Create and bootstrap web-server
+resource "aws_instance" "web-server" {
+  ami                         = data.aws_ssm_parameter.webserver-ami.value
+  instance_type               = "t3.micro"
+  key_name                    = aws_key_pair.webserver-key.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.sg.id]
+  subnet_id                   = aws_subnet.subnet.id
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm",
+      "sudo yum -y install ansible"
+    ]
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("~/.ssh/id_rsa")
+      host        = self.public_ip
+    }
+  }
+  tags = {
+    Name = "web-server"
+  }
+}
