2d5f7afe0ff0a2bdf9a12c61c15007529ec11ac4
Walton McDonald <walrussin@mailfence.com>
2024-04-28 22:29:33 -0600

    trying things

---
+++ provision/terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "aws_access_key_id" {
+  description = "The AWS Access Key ID"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_access_key" {
+  description = "The AWS Secret Access Key"
+  type        = string
+  default     = ""
+
+  
+}
